4c90e891635524ab2febdb9f05cbf2d8eed180e8
taroeld <tarob@live.de>
2023-01-07 10:14:54 +0100

    feat: add skeleton for eventual card creator screen

---
+++ src/BirdTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BirdTemplate(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BirdTemplate;
---
+++ src/CardCreatorScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CardCreatorScreen(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CardCreatorScreen;
