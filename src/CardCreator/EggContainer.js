a34d3461443bfda9f2090c46a201aed4aacaa0e0
taroeld <tarob@live.de>
2023-01-10 00:47:43 +0100

    feat: add EggContainer Component

---
+++ src/CardCreator/EggContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Egg from '../Egg';
+import "./EggContainer.css"
+
+const EggContainer = (props) => {
+    const getEggs = () => {
+        const ret = []
+        for (let index = 0; index < props.eggNum; index++) {
+            ret.push(<Egg/>)
+        }
+        return ret;
+    }
+    return (
+        <div className="eggContainer">
+            {getEggs()}
+        </div>
+    );
+};
+
+export default EggContainer;
