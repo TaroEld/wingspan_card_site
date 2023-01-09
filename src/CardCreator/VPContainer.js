2d1a26b3cb90de88b01352038937b0ca4629cc4d
taroeld <tarob@live.de>
2023-01-10 00:48:05 +0100

    feat: add VPContainer Component

---
+++ src/CardCreator/VPContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VPImg from "../assets/gfx/point.png";
+import "./VPContainer.css"
+
+const VPContainer = (props) => {
+    return (
+        <div className="vpContainer">
+            <span className="num">{props.vpNum}</span>
+            <img className="vpImg" src={VPImg} alt={VPImg}/>
+        </div>
+    );
+};
+
+export default VPContainer;
