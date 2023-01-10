00f3405b5ef6a6b83ccb93688ca7bbcdd4dec1c5
taroeld <tarob@live.de>
2023-01-10 15:25:02 +0100

    re: 87eec6251ffa41ab5605268841d2c6c424301972

--- src/Food.js
+++ src/Food.js
@@ -5,6 +5,7 @@ import Nectar from "./assets/gfx/food_nectar.png";
 import Seed from "./assets/gfx/food_seed.png";
 import Fruit from "./assets/gfx/food_cherry.png";
 import Fish from "./assets/gfx/food_fish.png";
+import styles from './Food.module.css'
 
 const FoodTypes = {
     Invertebrate : "Invertebrate",
@@ -27,12 +28,8 @@ const foodImages = {
 }
 const Food = (props) => {
     // const size = props.Size ? "size-" + props.Size : "size-normal"
-    const style= {
-        width: "1rem",
-        height: "1rem"
-    }
      return (
-        <div style={style}>
+        <div className={"foodImg " + styles.div}>
             <img className="containImage" src={foodImages[props.Type]} alt={props.Type}/>
         </div>
     );
