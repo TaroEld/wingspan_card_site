87eec6251ffa41ab5605268841d2c6c424301972
taroeld <tarob@live.de>
2023-01-10 15:24:12 +0100

    refactor: experiment with different versions of styling

--- src/CardCreator/FoodContainer.js
+++ src/CardCreator/FoodContainer.js
@@ -3,6 +3,11 @@ import "./FoodContainer.css"
 import {Food} from '../Food.js'
 
 function FoodContainer(_food) {
+    const style= {
+        width: "1rem",
+        height: "1rem",
+        display: "inline-block"
+    }
     const getFood = () => {
         // Either + or / depending if the bird requires multiple foods or can choose between multiple
         const separator = _food.separator;
@@ -10,12 +15,13 @@ function FoodContainer(_food) {
         // Iterate over foods and add each as component followed by separator character
         const ret = _food.foodArray.map((_foodType) => {
             const innerRet = []
-            innerRet.push(<Food key = {idx++} Type={_foodType} Size = "small"/>)
+            innerRet.push(<Food key = {idx++} style={style} Type={_foodType} Size = "small"/>)
             innerRet.push(<span key = {idx++} style={{color: "black", fontWeight :"bold"}}>{separator}</span>)
             return innerRet;
         })
         // Remove last separator
-        ret[ret.length - 1].pop()
+        if (ret.length > 0)
+            ret[ret.length - 1].pop()
         return ret
     }
     return (
