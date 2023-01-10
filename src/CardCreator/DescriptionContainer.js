332bd57369024591c079e25bf45a2e2b82b87d59
taroeld <tarob@live.de>
2023-01-10 15:23:17 +0100

    fix: remove commented line

--- src/CardCreator/DescriptionContainer.js
+++ src/CardCreator/DescriptionContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "./DescriptionContainer.css"
 import {Effect} from '../Effect.js';
 const DescriptionContainer = (props) => {
-    // text= {props.descriptionText} type={props.descriptionType} effect={props.descriptionEffect} tag={props.effectTag}
     function getDescriptionType()
     {
         switch(props.descriptionType)
