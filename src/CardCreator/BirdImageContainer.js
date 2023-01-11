4b26eb54cd11426f66e2b55ffb5e4af3c6684c97
taroeld <tarob@live.de>
2023-01-11 13:33:16 +0100

    feat: add BirdImageContainer

---
+++ src/CardCreator/BirdImageContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./BirdImageContainer.css"
+
+const BirdImageContainer = ({}) => {
+    return (
+        <div className="birdImageContainer">
+            
+        </div>
+    );
+};
+
+export default BirdImageContainer;
