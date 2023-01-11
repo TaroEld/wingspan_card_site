937224095ef21b3ae8f4c7acfab77e8f8d7b2ea7
taroeld <tarob@live.de>
2023-01-11 22:19:58 +0100

    refactor: misc styling improvement, particularly related to mobile view

--- src/Wingspan.js
+++ src/Wingspan.js
@@ -4,10 +4,10 @@ import WingspanImg from "./assets/gfx/wingspan.png";
 const Wingspan = () => {
     const style = {
         width: "5rem",
-        height: "1rem"
+        height: "1.5rem"
     }
     return (
-        <div style={style}>
+        <div className="wingspanImg" style={style}>
             <img className="containImage" src={WingspanImg} alt="Wingspan"/>
         </div>
     );
