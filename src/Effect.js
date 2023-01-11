dd951dcb57e9f3b19fdb08e9e5cd1317e7aa0aa1
taroeld <tarob@live.de>
2023-01-11 13:29:36 +0100

    feat: add Effect color map and fix up css

--- src/Effect.js
+++ src/Effect.js
@@ -10,16 +10,27 @@ import "./Effect.css"
 const EffectTypes = {
     None :      "None",
     Activated : "Activated",
+    EnemyTurn:  "EnemyTurn",
     Played :    "Played",
     Round :     "Round",
-    Game :      "Game",
-    Turn :      "Turn"
+    Game :      "Game"
+}
+
+const colorTypeMap = {
+    null: EffectTypes.None,
+    none: EffectTypes.None,
+    brown: EffectTypes.Activated,
+    white: EffectTypes.Played,
+    pink: EffectTypes.EnemyTurn,
+    teal: EffectTypes.Round,
+    yellow: EffectTypes.Game, 
 }
 
 const EffectTags = {
     None :      "None",
     Predator :  "Predator",
-    Tuck :      "Tuck",
+    Flocking :  "Flocking",
+    Bonus_Card: "Bonus_Card"
 } 
 
 const EffectImageKeyWordMap = {
@@ -36,24 +47,42 @@ for (const [key] of Object.entries(NestTypes)){
 }
 
 function Effect(props) {
+    // TODO: Add keys, I lack a good way to do that with the current way of getting the objects
     const prefixTypeMap = {
         None :      "",
         Activated : "WHEN ACTIVATED: ",
+        EnemyTurn: "ONCE BETWEEN TURNS: ",
         Played :    "WHEN PLAYED: ",
         Round :     "ROUND END: ",
         Game :      "GAME END: ",
-        Turn :      "TURN END: "
     }
+
     const prefixTagMap = {
         None : <span/>,
-        Tuck : <img className="containImage" src={Tuck} alt={Tuck}/>
+        Flocking : <img className="containImage" src={Tuck} alt={Tuck}/>,
+        Predator: <img className="containImage" src={Predator} alt={Predator}/>,
+        Bonus_Card: <img className="containImage" src={Tuck} alt={Tuck}/>,
+    }
+    const translateColorType = _type =>
+    {
+        if (_type in EffectTypes)
+            return _type
+        if (_type in colorTypeMap)
+            return colorTypeMap[_type]
+        console.error("Could not find effect type " + _type)
+        return _type
     }
     const composeDescription = (_text) => {
+        if (_text === null)
+            return <></>
         // This function goes over the text by splitting it into individual works based on spaces, then returns an array of either text spans, or image elements
+
+        // Effect types are frequently referred to by color
+        const type = translateColorType(props.type)
         const textFragments = _text.split(" ");
         const ret = [
             prefixTagMap[props.tag],
-            <span>{prefixTypeMap[props.type]}</span>
+            <span>{prefixTypeMap[type]}</span>
         ];
         // This variable is used to combine all the previous text snippets before an element to replace to combine them all into one <span>
         let subText = "";
@@ -73,10 +102,11 @@ function Effect(props) {
         // This makes sure no text is left behind (no element to replace / text after the last element)
         if (subText !== "")
             ret.push(<span className="text">{subText}</span>)  
+        
         return ret
     }
     return (
-        <div className={"effect " + props.type}>
+        <div className={"effect " + translateColorType(props.type)}>
             {composeDescription(props.text)}
         </div>
     );
