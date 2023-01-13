e7612f9361ef78b841ee40dc66e0cf630400fd83
taroeld <tarob@live.de>
2023-01-13 10:53:39 +0100

    refactor: sideBar gets children from App

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,14 +2,9 @@ import React from 'react';
 import './SideBar.css'
 
 function SideBar(props) {
-    const onButtonClicked = (_button) =>
-    {
-        props.parentCallback(_button)
-    }
     return (
         <div className="sideBar" >
-            <button className="cardCreator" onClick={() => onButtonClicked("CardCreator")}>Card Creator</button>
-            <button className="about" onClick={() => onButtonClicked("About")}>About</button>
+            {props.children}
         </div>
     );
 }
